81673f97bbd4081b03db917df014de6576a99d1e
Joel Shin <jwshin@gmail.com>
2013-11-09 17:28:03 -0800

    rfactrd test

--- spec/rig_details_formatter_spec.rb
+++ spec/rig_details_formatter_spec.rb
@@ -11,54 +11,16 @@ end
 describe "RigDetailsFormatter" do
  	
 	describe "when complete data with downtime, collecting and commuting" do
-		before { @f = RigDetailsFormatter.new(myhashes) }
-		describe "downtime_distance" do
- 			it "returns 111.1" do
- 				@f.downtime_distance.should == '111.1'
- 			end
- 		end
-		describe "downtime_duration" do
- 			it "returns 11:11" do
- 				@f.downtime_duration.should == '11:11'
- 			end
- 		end
- 		describe "downtime_update_count" do
- 			it "returns 11" do
- 				@f.downtime_update_count.should == '11'
- 			end
- 		end
-
- 		describe "collecting_distance" do
- 			it "returns 111.1" do
- 				@f.collecting_distance.should == '777.7'
- 			end
- 		end
-		describe "collecting_duration" do
- 			it "returns 77:77" do
- 				@f.collecting_duration.should == '77:77'
- 			end
- 		end
- 		describe "collecting_update_count" do
- 			it "returns 11" do
- 				@f.collecting_update_count.should == '77'
- 			end
- 		end
-
-  		describe "commuting_distance" do
- 			it "returns 22.2" do
- 				@f.commuting_distance.should == '22.2'
- 			end
- 		end
-		describe "commuting_duration" do
- 			it "returns 22:22" do
- 				@f.commuting_duration.should == '22:22'
- 			end
- 		end
- 		describe "commuting_update_count" do
- 			it "returns 22" do
- 				@f.commuting_update_count.should == '22'
- 			end
- 		end						
+		subject { RigDetailsFormatter.new(myhashes) }
+ 		its(:downtime_distance) { should == '111.1' }
+ 		its(:downtime_duration)  { should == '11:11' }
+ 		its(:downtime_update_count) { should == '11' }
+ 		its(:collecting_distance) { should == '777.7' }
+ 		its(:collecting_duration) { should == '77:77' }
+ 		its(:collecting_update_count) { should == '77' }
+ 		its(:commuting_distance) { should == '22.2' }
+ 		its(:commuting_duration) { should == '22:22' }
+ 		its(:commuting_update_count) { should == '22' }
  	end
 
 end
