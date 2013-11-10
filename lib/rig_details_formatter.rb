5b9e8fbeb2b5751338fa9249fd37cb2a79b44c57
Joel Shin <jwshin@gmail.com>
2013-11-10 13:00:09 -0800

    done

--- lib/rig_details_formatter.rb
+++ lib/rig_details_formatter.rb
@@ -3,10 +3,13 @@ require 'date'
 class RigDetailsFormatter
 	attr_accessor :start_time, :end_time, :downtime_duration, :downtime_distance, :downtime_update_count, :commuting_duration, :commuting_distance, :commuting_update_count, :collecting_duration, :collecting_distance, :collecting_update_count
 
+	RIG_STATUSES = ['downtime', 'collecting', 'commuting']
+	RIG_STATUS_ATTRIBUTES = ['duration', 'distance', 'update_count']
+
 	def initialize(data)
 		@start_time, @end_time = 'NA', 'NA'
 		if data.empty?
-			['downtime', 'collecting', 'commuting'].each { |status| set_elements_to_not_available status }
+			RIG_STATUSES.each { |status| set_elements_to_not_available status }
 		elsif data.is_a?(Array) && data.length > 1
 			data.each do |data_item| 
 				set_elements_to_value data_item
@@ -14,13 +17,9 @@ class RigDetailsFormatter
 				set_end_time(data_item) if is_not_the_latest_end_time?(data_item[:end_time_utc])
 			end
         else # just one hash in an array
-			['collecting', 'commuting', 'downtime'].each do |status|
-				if data[0][:collection_status] == status
-					set_elements_to_value data[0]
-				end
-				if data[0][:collection_status] != status
-					set_elements_to_not_available status
-				end
+			RIG_STATUSES.each do |status|
+				set_elements_to_value( data[0] ) if data[0][:collection_status] == status				
+				set_elements_to_not_available( status ) if data[0][:collection_status] != status
 			end
 			set_start_time data[0]
 			set_end_time data[0]
@@ -46,11 +45,11 @@ class RigDetailsFormatter
 		@end_time = as_datetime_obj data_item[:end_time_utc]
 	end	
 	def set_elements_to_value info
-		['duration', 'distance', 'update_count'].each do |elem| 
-			send "#{info[:collection_status]}_#{elem}=", info["#{elem}".to_sym] 
+		RIG_STATUS_ATTRIBUTES.each do |attribute| 
+			send "#{info[:collection_status]}_#{attribute}=", info["#{attribute}".to_sym] 
 		end
 	end
 	def set_elements_to_not_available( status )
-		['duration', 'distance', 'update_count'].each { |element| send "#{status}_#{element}=", 'NA' }
+		RIG_STATUS_ATTRIBUTES.each { |attribute| send "#{status}_#{attribute}=", 'NA' }
 	end
 end
